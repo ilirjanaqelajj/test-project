0b5db35367c6b8547777cadae9f3096fdea89231
ilirjanaqelajj <96169288+ilirjanaqelajj@users.noreply.github.com>
2024-01-10 11:50:12 +0100

    added 1 endpoint

---
+++ src/main/java/com/assist/java/demo/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.assist.java.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Controller {
+
+    @GetMapping
+    public String getString(){
+        return "test111";
+    }
+}
--- src/main/java/com/assist/java/demo/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.assist.java.demo.service;
-
-import com.assist.java.demo.entity.ApplicationUser;
-
-public abstract class UserService {
-	public abstract boolean saveUser(ApplicationUser user);
-	public abstract ApplicationUser getUserByUsername(String username);	
-}
