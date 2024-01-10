e9601d76214ce309b142749ebe2c32b053983169
ilirjanaqelajj <96169288+ilirjanaqelajj@users.noreply.github.com>
2024-01-10 14:08:09 +0100

    fix #1

--- src/main/java/com/assist/java/demo/MultiTenancyApplication.java
+++ src/main/java/com/assist/java/demo/MultiTenancyApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootApplication
-@SpringBootTest
 public class MultiTenancyApplication {
 
   public static void main(String[] args) {
