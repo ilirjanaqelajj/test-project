f1605ce505f682ee2770c7e32ed297be74780fcc
ilirjanaqelajj <96169288+ilirjanaqelajj@users.noreply.github.com>
2024-02-23 13:27:21 +0100

    minor

--- src/main/java/com/assist/java/demo/MultiTenancyApplication.java
+++ src/main/java/com/assist/java/demo/MultiTenancyApplication.java
@@ -1,11 +1,13 @@
 package com.assist.java.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+@SpringBootConfiguration
 public class MultiTenancyApplication {
 
   public static void main(String[] args) {
